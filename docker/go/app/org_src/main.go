070affdf06c89e184bf33ea386e72ae06d0649ad
PON_MAEDA <riotam.engineer@gmail.com>
2022-03-02 20:59:07 +0900

    以下対応 - MySQLのコンテナ追加 - gormを追加 - appとdbの接続確認済み

--- docker/go/app/org_src/main.go
+++ docker/go/app/org_src/main.go
@@ -1,14 +1,52 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"net/http"
+)
 
-import "net/http"
+type TableSample struct {
+	Id   int    `gorm:"column:id"`
+	Name string `gorm:"column:name"`
+}
+
+func gormConnect() *gorm.DB {
+	DBMS := "mysql"
+	USER := "docker"
+	PASS := "docker"
+	PROTOCOL := "tcp(db:3306)"
+	DBNAME := "sample"
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
 
 func main() {
+
+	// MySQLとの接続
+	db := gormConnect()
+	defer db.Close()
+
 	engine := gin.Default()
 	engine.GET("/", func(c *gin.Context) {
+
+		// データ取得
+		var tableSample []TableSample
+		db.Table("table_sample").Find(&tableSample)
+
+		fmt.Println("tableSample: ", tableSample)
+
 		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world!! I'm PON!!!",
+			"message": "success!",
+			"data":    tableSample,
 		})
 	})
 	engine.Run(":3000")
