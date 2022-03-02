44493d91f9a892f72252e59b37a28e1305f186f7
PON_MAEDA <riotam.engineer@gmail.com>
2022-03-02 17:00:15 +0900

    MySQLをコンテナ化する前提でgoのディレクトリ構成変更

--- docker/go/app/org_src/main.go
+++ docker/go/app/org_src/main.go
@@ -8,7 +8,7 @@ func main() {
 	engine := gin.Default()
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world! I'm PON!",
+			"message": "hello world!! I'm PON!!!",
 		})
 	})
 	engine.Run(":3000")
