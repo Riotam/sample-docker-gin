431b664ba0c9e34ad0a7e27bb179d0ff3114714f
PON_MAEDA <riotam.engineer@gmail.com>
2022-03-11 07:31:44 +0900

    サンプルのAPIにクエリパラメータを渡すように修正

--- docker/go/internal/handler/samples.go
+++ docker/go/internal/handler/samples.go
@@ -4,11 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"sample-docker-gin/internal/util"
+	"strconv"
 )
 
 // Sample はスケルトンコード
 func Sample(c *gin.Context) {
-	res := util.Sample(1, 2)
+
+	aStr := c.Query("a")
+	bStr := c.Query("b")
+
+	aInt, err := strconv.Atoi(aStr)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+		return
+	}
+
+	bInt, err := strconv.Atoi(bStr)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+		return
+	}
+
+	res := util.Sample(aInt, bInt)
 
 	c.JSON(http.StatusOK, res)
 }
