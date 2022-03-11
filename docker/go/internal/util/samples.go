7e747cbc61bc51ad60610ba3907ceb288cd8f3fa
PON_MAEDA <riotam.engineer@gmail.com>
2022-03-12 06:18:18 +0900

    .circleci/config.ymlの作成

--- docker/go/internal/util/samples.go
+++ docker/go/internal/util/samples.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func Sample(a int, b int) string {
 	res := a + b
-	return fmt.Sprintf("from util.Sample! with %d", res)
+	return fmt.Sprintf("Hi! from util.Sample! with %d", res)
 }
--- docker/go/test/util/samples_test.go
+++ docker/go/test/util/samples_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestSamples_Sample(t *testing.T) {
 	actual := util.Sample(1, 2)
-	expected := "from util.Sample! with 3"
+	expected := "Hi! from util.Sample! with 3"
 	if actual != expected {
 		t.Errorf("got %v\nwant %v", actual, expected)
 	}
